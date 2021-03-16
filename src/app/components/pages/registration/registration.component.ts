30ffbdca904a9831dfc9f2a5a6b53535dbad9fa9
Naveen B <naveen.bodicherla@thermofisher.com>
2021-03-16 20:27:47 +0530

    Registration changes

--- src/app/components/pages/registration/registration.component.ts
+++ src/app/components/pages/registration/registration.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import {
+  UserService
+} from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +18,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private userS: UserService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: this.fb.control('', Validators.required),
+      lastName: this.fb.control('', Validators.required),
+      userEmail: this.fb.control('', Validators.required),
+      password: this.fb.control('', Validators.required),
+      confirmPassword: this.fb.control('', Validators.required),
+      companyName: this.fb.control('', Validators.required),
+      location: this.fb.control('', Validators.required),
+      userRole: this.fb.control('', Validators.required),
+      invoiceCompanyName: this.fb.control('', Validators.required),
+      businessRegistrationNo: this.fb.control('', Validators.required),
+      invoiceEmail: this.fb.control('', Validators.required),
+      phoneNumber: this.fb.control('', Validators.required),
+      businessAddress: this.fb.control('', Validators.required),
+    });
+  }
+
+  registerUser = () => {
+    const payload = this.registerForm.value;
+    this.userS.registerUser(payload)
+      .subscribe((e) => {
+        console.log(e);
+      });
+
   }
 
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@angular/core';
-import { LoginForm } from 'src/app/models/forms.model';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { DatastoreService } from '../data-store/data-store.service';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  LoginForm
+} from 'src/app/models/forms.model';
+import {
+  Observable
+} from 'rxjs';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  map
+} from 'rxjs/operators';
+import {
+  Router
+} from '@angular/router';
+import {
+  DatastoreService
+} from '../data-store/data-store.service';
 
 @Injectable({
   providedIn: 'root',
@@ -20,19 +34,25 @@ export class UserService {
     return this.ds.getStore('isLoggedin');
   };
 
-  authenticateUser = (form: LoginForm): Observable<any> => {
+  authenticateUser = (form: LoginForm): Observable < any > => {
     const url = '/api/login';
 
     const payload = {
       userEmail: form.email,
       userPassword: form.password,
     };
-    return this.http.post(url, payload).pipe(
-      map((response: any) => {
-        return response;
-      })
-    );
+    return this.http.post(url, payload)
+      .pipe(
+        map((response: any) => {
+          return response;
+        })
+      );
   };
+
+  registerUser = (payload: object): Observable < any > => {
+    const url = "/api/registration";
+    return this.http.post(url, payload);
+  }
 
   logoutUser = (): void => {
     const ls = localStorage;
